f20fa021049d343ec11432835d4bdaa72cb233cb
Dhruv Patel <142231508+dhruvpatel99269@users.noreply.github.com>
2025-07-19 21:05:46 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello from Dockerized Jenkins CI/CD!\nIm Pranay Vasoya and this is my first Jenkins pipeline.\n');
+    res.end('Hello from Dockerized Jenkins CI/CD!\nIm Dhruv Patel and this is my first Jenkins pipeline.\n');
 });
 
 server.listen(port, hostname, () => {
